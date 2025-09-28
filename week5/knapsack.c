337aa7277aaa2c23fc7de4c5cd03f9e2dcc30d26
Aarushi <aarushi2005in@gmail.com>
2025-09-28 17:12:48 +0530

    Update knapsack.c

--- week5/knapsack.c
+++ week5/knapsack.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
 
-// Function to return maximum of two numbers
+// Function to get maximum of two integers
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-// Function to solve 0/1 Knapsack problem
+// Recursive function for 0/1 Knapsack
 int knapsack(int W, int wt[], int val[], int n) {
-    int dp[n + 1][W + 1];
-
-    // Build table dp[][] bottom-up
-    for (int i = 0; i <= n; i++) {
-        for (int w = 0; w <= W; w++) {
-            if (i == 0 || w == 0)
-                dp[i][w] = 0;  // Base case
-            else if (wt[i - 1] <= w)
-                dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
-            else
-                dp[i][w] = dp[i - 1][w];
-        }
-    }
-
-    return dp[n][W];  // Maximum value
+    if (n == 0 || W == 0)
+        return 0;
+    if (wt[n - 1] > W)
+        return knapsack(W, wt, val, n - 1);
+    else
+        return max(
+            val[n - 1] + knapsack(W - wt[n - 1], wt, val, n - 1),
+            knapsack(W, wt, val, n - 1)
+        );
 }
 
 int main() {
-    int values[] = {60, 100, 120};   // Values of items
-    int weights[] = {10, 20, 30};    // Weights of items
-    int capacity = 50;               // Knapsack capacity
-    int n = sizeof(values) / sizeof(values[0]);
+    int n, W, i;
+    printf("Enter number of items: ");
+    scanf("%d", &n);
 
-    printf("Maximum value in Knapsack = %d\n", knapsack(capacity, weights, values, n));
+    int val[n], wt[n];
+    printf("Enter values of items:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &val[i]);
+
+    printf("Enter weights of items:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &wt[i]);
+
+    printf("Enter capacity of knapsack: ");
+    scanf("%d", &W);
+
+    int result = knapsack(W, wt, val, n);
+    printf("Maximum value in knapsack = %d\n", result);
 
     return 0;
 }
